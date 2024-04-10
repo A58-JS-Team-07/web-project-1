3b8f93e4633684f47a93d141bff134a83453ff63
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-04-10 22:37:17 +0300

    Added api.js file with API constants + constant.js with shortcodes. Additinally, npm and eslint installed.

---
+++ src/common/constants.js
@@ -0,0 +1,11 @@
+export const TRENDING = 'trending';
+
+export const FAVORITES = 'favorites';
+
+export const UPLOAD = 'upload';
+
+export const CONTAINER_SELECTOR = '#container';
+
+export const FULL_HEART = '❤';
+
+export const EMPTY_HEART = '♡';
