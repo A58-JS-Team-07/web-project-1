81fb6b29031a4a002f57b6bb7f5d59a4a7b5a4ca
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-04-11 19:00:05 +0300

    trending-view.js added html functions for displaying trending page

--- src/views/trending-view.js
+++ src/views/trending-view.js
@@ -1,13 +1,15 @@
-export const toTrendingView = (pageTitle, pageDescription, GIFsArray) => `
-<h1 id="page-title">${pageTitle}</h1>
-<p id="">${pageDescription}</p>
-<div class="">${GIFsArray.map().join('\n')}</div>
-`;
+export const toTrendingView = (pageTitle, pageDescription, GIFsArray) => {
+  return `<h1 id="page-title">${pageTitle}</h1>
+  <p id="">${pageDescription}</p>
+  <div class="gifs-listing">${GIFsArray.map(toMovieSimple).join('\n')}</div>`;
+};
 
 export const toMovieSimple = (GIF) => `
 <div class="gif-snippet">
-  <!-- ${renderFavoriteStatus(GIF.id)} THIS MUST BE corrected !!!!!!!!!!!!!! -->
   <img class="gif-image" src="${GIF.image.url}" alt="${GIF.title}" width=100% height=auto>
   <h2 class="gif-title">${GIF.title}</h2>
 </div>
 `;
+
+
+// <!-- ${renderFavoriteStatus(GIF.id)} THIS MUST BE corrected !!!!!!!!!!!!!! -->
