37c76d2355d1d72634a83ac53efc73b7424c931f
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-04-11 14:21:57 +0300

    request-services.js added loadGIFsBySearchTerm  API request function

--- src/requests/request-service.js
+++ src/requests/request-service.js
@@ -42,7 +42,7 @@ export const loadSingleGIFbyID = async (id) => {
     const response = await fetch(`https://api.giphy.com/v1/gifs/${id}?api_key=${API_MASTER}&rating=g`);
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch trending gifs: ${response.message}`);
+      throw new Error(`Failed to fetch gif by ID: ${response.message}`);
     }
 
     const singleGIFBody = await response.json();
@@ -76,7 +76,7 @@ export const loadMultipleGIFsByID = async (arrayWithGIFsID) => {
     const response = await fetch(`https://api.giphy.com/v1/gifs?api_key=${API_MASTER}&ids=${arrayWithGIFsID.join(',')}&rating=g`);
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch trending gifs: ${response.message}`);
+      throw new Error(`Failed to fetch gifs by IDs: ${response.message}`);
     }
 
     const multipleGIFsBody = await response.json();
@@ -102,12 +102,43 @@ export const loadMultipleGIFsByID = async (arrayWithGIFsID) => {
   }
 };
 
+export const loadGIFsBySearchTerm = async (query, limit = '25') => {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_MASTER}&q=${query}&limit=${limit}&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
 
-// (async () => {
-//   try {
-//     const result = await loadMultipleGIFsByID(['yopWhLoxazBTbVFSIc', 'QYK37DfxZioqeNnOgX', 'V42XDIEuOIitfEE5n5']);
-//     console.log('Async operation completed:', result);
-//   } catch (error) {
-//     console.error('An error occurred during async operation:', error);
-//   }
-// })();
+    if (!response.ok) {
+      throw new Error(`Failed to fetch searched gifs: ${response.message}`);
+    }
+
+    const searchGIFsBody = await response.json();
+    const searchGIFsArray = searchGIFsBody.data;
+
+    const resultArray = [];
+
+    searchGIFsArray.map((gif) => {
+      const gifObject = {
+        title: gif.title,
+        id: gif.id,
+        image: {
+          url: gif.images.original.url,
+          height: gif.images.original.height,
+          width: gif.images.original.width,
+        },
+      };
+      resultArray.push(gifObject);
+    });
+    return resultArray;
+  } catch (e) {
+    throw new Error(`Error in loadGIFsBySearchTerm async function: ${e.message}`);
+  }
+};
+
+
+(async () => {
+  try {
+    const result = await loadGIFsBySearchTerm('cats', '5');
+    console.log('Async operation completed:', result);
+  } catch (error) {
+    console.error('An error occurred during async operation:', error);
+  }
+})();
