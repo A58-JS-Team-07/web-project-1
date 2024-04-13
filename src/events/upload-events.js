24a60722adb1775e94a9678ac9718cd22ca1858a
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-04-13 11:30:43 +0300

    Upload functionally 100% working

--- src/events/upload-events.js
+++ src/events/upload-events.js
@@ -2,15 +2,20 @@ import { uploadGIF } from '../requests/request-service.js';
 
 export const executeUploadItem = async (formData) => {
   const stringTags = formData.get('tags') || '';
-  const file = /*formData.get('local-file') || */ null;
+  let file = formData.get('local-file');
   const fileURL = formData.get('file-url') || '';
   const sourcePostURL = formData.get('source-url') || '';
 
-  console.log(stringTags);
-  console.log(file);
-  console.log(fileURL);
-  console.log(sourcePostURL);
-  // Call uploadGIF function with extracted data
+  // console.log(stringTags);
+  // console.log(file.name);
+  // console.log(file);
+  // console.log(fileURL);
+  // console.log(sourcePostURL);
+
+  if (file.name === '') {
+    file = '';
+  }
+
   try {
     await uploadGIF(stringTags, file, fileURL, sourcePostURL);
     console.log('Upload successful!');
