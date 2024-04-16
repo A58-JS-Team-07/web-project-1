0b95d1f6e2f9984fd5143e7e0fd20ad4fd7749d1
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-04-16 16:04:27 +0300

    Magic number fix

--- src/common/constants.js
+++ src/common/constants.js
@@ -6,6 +6,8 @@ export const UPLOAD = 'upload';
 
 export const MY_UPLOAD = 'my-uploads';
 
+export const SNIPPETS_LIMITER = '25';
+
 export const CONTAINER_SELECTOR = '#container';
 
 export const FULL_HEART = '❤';
