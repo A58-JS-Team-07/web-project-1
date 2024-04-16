84b2c276c390f396908044053a38bb691b6d0efe
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-04-16 17:45:14 +0300

    Upload GIF loader complete

--- src/events/upload-form-events.js
+++ src/events/upload-form-events.js
@@ -1,5 +1,6 @@
 import { uploadGIF } from '../requests/request-service.js';
 import { addUpload } from '../data/uploads.js';
+import { setLoader } from './helpers.js';
 
 export const executeUploadItem = async (formData) => {
   const stringTags = formData.get('tags') || '';
@@ -15,8 +16,10 @@ export const executeUploadItem = async (formData) => {
     const newGifId = await uploadGIF(stringTags, file, fileURL, sourcePostURL);
     addUpload(newGifId);
     console.log('Upload successful!');
+    setLoader('stop');
     alert('Upload successful!');
   } catch (error) {
+    setLoader('stop');
     console.error('Error uploading GIF:', error.message);
     alert('Please provide a GIF with the following formats: GIF, MP4, MOV, WebM or valid media URL.');
   }
